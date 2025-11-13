c1af5ce82a253d810c3d77f81a1d6be9760d3636
jakub7038 <162906494+jakub7038@users.noreply.github.com>
2025-11-13 16:36:47 +0100

    Menu buttons early version

--- app/src/main/java/com/example/gesturepulse/MainActivity.kt
+++ app/src/main/java/com/example/gesturepulse/MainActivity.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gesturepulse.ui.theme.GesturePulseTheme
+import androidx.compose.ui.graphics.Color
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +34,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GesturePulseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//                raczej nie bede potrzebowac paska cofania
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainMenuScreen()
                 }
             }
         }
@@ -31,17 +54,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainMenuScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        MenuButton(
+            text = "Graj",
+            onClick = { /* TODO: logika */ }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MenuButton(
+            text = "Rankingi",
+            onClick = { /* TODO: logika */ }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MenuButton(
+            text = "Ustawienia",
+            onClick = { /* TODO: logika */ }
+        )
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MenuButton(
+            text = "Exit",
+            onClick = { /* TODO: logika */ }
+        )
+    }
+}
+
+@Composable
+fun MenuButton(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    val buttonModifier = modifier.fillMaxWidth()
+        .padding(horizontal = 32.dp)
+        .height(60.dp)
+
+    val buttonShape = RoundedCornerShape(16.dp)
+
+    val colors = ButtonDefaults.buttonColors(
+        containerColor = Color(0xFFFF5A22),
+        contentColor = Color(0xFFFFFFFF)
     )
+
+    val fontWeight = FontWeight.Bold
+
+    Button(
+        onClick = onClick,
+        modifier = buttonModifier,
+        shape = buttonShape,
+        colors = colors
+    ) {
+        Text(text, fontSize = 20.sp, fontWeight = fontWeight)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GesturePulseTheme {
-        Greeting("Android")
+        MainMenuScreen()
     }
 }
